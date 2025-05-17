d51c71c7fa77ca68132f677f9c4a6c32d6d3117c
saurabh <saurabhyadav42399@gmail.com>
2025-05-17 20:00:41 +0530

    added mainnet details info

--- script/PoolManagerFactory.s.sol
+++ script/PoolManagerFactory.s.sol
@@ -8,36 +8,26 @@ import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
 import {PoolManager} from "../src/PoolManager.sol";
 
 contract DeployPoolManagerFactory is Script {
-    address owner = address(0x641BB2596D8c0b32471260712566BF933a2f1a8e);
-    address strategyManager = address(0x641BB2596D8c0b32471260712566BF933a2f1a8e);
-    ERC20Mock public mockWETH;
-    ERC20Mock public mockUSDC;  
-    ERC20Mock public mockDAI;
-    ERC20Mock public mockUSDT;
+    address owner = address(0xE4f3B256c27cE7c76C5D16Ae81838aA14d8846C8);
+    address strategyManager = address(0xE4f3B256c27cE7c76C5D16Ae81838aA14d8846C8);
+    address WETH =0x4200000000000000000000000000000000000006;
+    address USDC =0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+    address DAI =0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb;
+    address USDT =0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2;
     
 
     function deployPoolManagerAndTokens() public returns (address) {
 
         PoolManager poolManager = new PoolManager();
 
-        mockWETH = new ERC20Mock("WETH", "WETH", 18);
-        mockUSDC = new ERC20Mock("USDC", "USDC", 6);
-        mockDAI = new ERC20Mock("DAI", "DAI", 18);
-        mockUSDT = new ERC20Mock("USDT", "USDT", 6);    
-
-        vm.label(address(mockWETH), "MockWETH");
-        vm.label(address(mockUSDC), "MockUSDC");
-        vm.label(address(mockDAI), "MockDAI");
-        vm.label(address(mockUSDT), "MockUSDT");
-
-        bytes memory data = abi.encodeWithSelector(PoolManager.initialize.selector,owner,strategyManager);
+        bytes memory data = abi.encodeWithSelector(PoolManager.initialize.selector,owner,strategyManager,address(12));
 
         for(uint i=0;i<3;i++){
             ERC1967Proxy proxy = new ERC1967Proxy(address(poolManager), data);
-            PoolManager(address(proxy)).registerAsset(address(mockWETH), "WETH", "WETH");
-            PoolManager(address(proxy)).registerAsset(address(mockUSDC), "USDC", "USDC");
-            PoolManager(address(proxy)).registerAsset(address(mockDAI), "DAI", "DAI");
-            PoolManager(address(proxy)).registerAsset(address(mockUSDT), "USDT", "USDT");
+            PoolManager(address(proxy)).registerAsset(WETH, "WETH", "WETH");
+            PoolManager(address(proxy)).registerAsset(USDC, "USDC", "USDC");
+            PoolManager(address(proxy)).registerAsset(DAI, "DAI", "DAI");
+            PoolManager(address(proxy)).registerAsset(USDT, "USDT", "USDT");
 
             vm.label(address(proxy), "PoolManagerProxy");
         }
